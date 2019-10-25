abcdf89e30c4dea5e3b377e2b2059a2221c18ce2
201816040322 <56496517+201816040322@users.noreply.github.com>
2019-10-25 19:07:02 +0800

    Create Ex07_14.cpp
    This is my Assianment_02
    My Student ID is 201816040322

---
+++ 201816040322/Ex07_14.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    const int N=20;//Define constant N, easy to modify the length of read data
+    int temp,flag,i;//A flag variable
+    vector <int> items;//Defined vector object
+
+    cout << "Please enter " << N << " numbers between 10 and 100 (including 10 and 100) and each number is separated by a space:" <<endl;
+    for(i=0;i<N;i++)
+    {
+        cin >> temp;
+        if(temp>100||temp<10)//Determine the validity of the data and mark its location
+        {
+            cout << "The number " << temp << " is invalid!" <<endl;
+            continue;//The code does not execute after the data is invalid
+        }
+        flag=0;
+        for(int item:items)
+        {
+            if(item==temp)
+            {
+                flag=1;//Tagged repeating element
+                break;//Reduce cycle times
+            }
+        }
+        if(flag==0)//Insert non-repeating elements
+        {
+            items.push_back(temp);
+        }
+    }//The end of the input
+
+    cout << "\nThe different numbers you entered are:" <<endl;
+    for(int item:items)//Traverse the output
+    {
+        cout << item <<" ";
+    }
+
+    return 0;
+}
