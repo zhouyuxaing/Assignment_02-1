d85bcb2b2ce2d03ba9f242d9bc0253a73868843a
今夕何夕 <53158211+LXGTLHL@users.noreply.github.com>
2019-10-25 22:02:50 +0800

    Update Ex07_14.cpp
    This is my assignment
    Student ID:201816040220

--- 201816040220/Ex07_14.cpp
+++ 201816040220/Ex07_14.cpp
@@ -2,40 +2,40 @@
 #include <vector>
 
 using namespace std;
-void push_back( vector <int> &);
+
+
 int main()
 {
-    vector<int> i(20);                 //定义一个空的vector数组
-    push_back(i);                     //进入push_back函数
-    return 0;
-}
-void push_back( vector <int> &items)
-{
-   int i,x,flag=1,j=1;
-   for(i=0;i<20;i++)
+    vector<int> items(1);
+    int j=0,k,flag=1,m,x;
+
+     for(m=0;m<20;m++)
    {
        cin>>x;
-        for(int item:items)           //从输入的第一个数开始，遍历，如果相同
-        {                             //flag=0，退出循环
-            if(x==item)
-            {
-                flag=0;
-                break;
-            }
-        }
-        if(flag==1)                  //flag=1，把x放入array数组
-        {
-           items[j]=x;
-           j++;
-        }
-        if(flag==0)                 //把flag变为0，下次循环再用
-            flag=1;
+       j++;               
 
+      if(m==0)              
+        items[0]=x;                     //把第一个数赋给items
+
+       else{
+        for(k=0;k<j;k++)                 //将读取的数逐个与现有的数比较
+          {
+             if(x==items[k])                 //如果相同flag=0
+             {
+                 flag=0;
+                 break;
+             }
+          }
+       }
+       if(flag==1&&m>0)                   //不让第一个数push  
+       items.push_back(x);              //把数push_back到items
+
+       else flag=1;
     }
-    for(int item:items)            //输出
-    {
-        if(item>=10)
+
+     for(int item:items)            //输出
+
        cout <<item<<endl;
-    }
 
+    return 0;
 }
