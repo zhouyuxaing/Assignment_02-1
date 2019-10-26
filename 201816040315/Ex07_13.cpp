11ad224f16811d47016025c99bc3817654b23098
唐少旭 <2399592661@qq.com>
2019-10-26 10:19:05 +0800

    Create Ex07_13.cpp&&Ex07_14.cpp

---
+++ 201816040315/Ex07_13.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <array>//包含头文件<array>
+
+using namespace std;
+
+int main()
+{
+    array< int, 20> duplicate={0};//声明array对象
+    int i,x,j;
+    cout<<"请输入20个10~100的数字（包含10和100）: "<<endl;
+
+    //读入20个数到array
+    for(i=0;i<20;i++)
+    {
+        cin>>x;//输入
+        if(x>=10&&x<=100)//判断输入数据是否合乎要求
+         duplicate[i]=x;   //合乎要求的数据存入
+    }
+
+    //检查是否有重复数据，若有重复数据则取一个
+    for(i=0;i<20;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            if(duplicate[i]==duplicate[j])
+                duplicate[i]=0;
+        }
+    }
+
+    //输出array中的数据
+    for(int element:duplicate)
+    {
+        if(element!=0)
+        cout<<element<<" "<<endl;
+    }
+
+    return 0;
+}
